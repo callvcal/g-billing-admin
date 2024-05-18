0160b960de44377d5c0d10df8f9f131d28654362
Callvcal <tech.callvcal@gmail.com>
2024-05-18 14:59:40 +0530

    Schema::dropIfExists('in_app_purchases');

--- database/migrations/2024_05_10_091526_create_in_app_purchases_table.php
+++ database/migrations/2024_05_10_091526_create_in_app_purchases_table.php
@@ -13,12 +13,17 @@ class CreateInAppPurchasesTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('in_app_purchases');
         Schema::create('in_app_purchases', function (Blueprint $table) {
             $table->id();
             $table->json('json')->nullable();
             $table->string('status')->nullable();
             $table->string('product_id')->nullable();
             $table->string('purchase_id')->nullable();
+            $table->unsignedBigInteger('admin_id')->nullable();
+            $table->foreign('admin_id')->references('id')->on('admin_users')->nullOnDelete();
+            $table->unsignedBigInteger('business_id')->nullable();
+            $table->foreign('business_id')->references('id')->on('businesses')->nullOnDelete();
             $table->timestamps();
         });
     }
