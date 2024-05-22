d16f1e94e365ddf89dab6c068617d2e40b7db60f
Callvcal <tech.callvcal@gmail.com>
2024-05-22 13:36:24 +0530

    fillable

--- app/Models/OfflineTransaction.php
+++ app/Models/OfflineTransaction.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class OfflineTransaction extends Model
 {
     protected $table = 'offline_transactions';
+    
+    
+    protected $fillable = [
+        'id',
+        'amount',
+        'type',
+        'cause',
+        'business_id',
+        'admin_id',
+
+    ];
 }
