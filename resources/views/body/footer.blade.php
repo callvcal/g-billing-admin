6f2b13559c189019bdf9e24c57e295a2d8cfdbf6
Sonu Kumar <sonukumarsweet12@gmail.com>
2024-05-27 19:40:48 +0530

    page update

--- resources/views/body/footer.blade.php
+++ resources/views/body/footer.blade.php
@@ -6,42 +6,44 @@
 
   <footer class="footer">
 
-    <div class="footer-top" style="{{ asset('img/footer-illustration.png') }}">
+    <div class="footer-top" style="{{ asset('public/img/footer.png') }}">
       <div class="container">
 
         <div class="footer-brand">
 
          
-    <a href="/" class="logo">
-    <img src="{{ asset('img/logos.png') }}" alt="Eatplan8" class="logo-image" style="width: 150px; height: auto;">
-</a>
+
+        <a href="" class="logo">
+       <img src="{{ asset('public/img/icon.png') }}" alt="Eatinsta Logo" style="width: 150px; height: auto;">
+        </a>
+ 
 
           <p class="footer-text">
-          Elevating Patna's culinary landscape, our restaurant crafts unforgettable experiences through authentic flavors and warm hospitality.
+          Effortless Billing & Management for Restaurants, Sweet Shops, Groceries & More
           </p>
 
           <ul class="social-list">
 
             <li>
-              <a href="#" class="social-link">
+              <a href="/" class="social-link">
                 <ion-icon name="logo-facebook"></ion-icon>
               </a>
             </li>
 
             <li>
-              <a href="#" class="social-link">
+              <a href="/" class="social-link">
                 <ion-icon name="logo-twitter"></ion-icon>
               </a>
             </li>
 
             <li>
-              <a href="#" class="social-link">
+              <a href="https://www.instagram.com/im_Eatinsta/" class="social-link">
                 <ion-icon name="logo-instagram"></ion-icon>
               </a>
             </li>
 
             <li>
-              <a href="#" class="social-link">
+              <a href="https://www.instagram.com/im_Eatinsta/" class="social-link">
                 <ion-icon name="logo-pinterest"></ion-icon>
               </a>
             </li>
@@ -58,19 +60,19 @@
           </li>
 
           <li>
-            <p class="footer-list-item"> 9102424888</p>
+            <p class="footer-list-item"> Wp: 8309900330</p>
           </li>
           
           <li>
-            <p class="footer-list-item"> 9102524888</p>
+            <p class="footer-list-item">Mb: 7033879015</p>
           </li>
 
           <li>
-            <p class="footer-list-item">eatinsta@gmail.com</p>
+            <p class="footer-list-item">Eatinsta@gmail.com</p>
           </li>
 
           <li>
-            <address class="footer-list-item">Gayatri Palace,Adarsh Colony Chowk,West Patel Nagar,Near Dakshin Bihar Gramin Bank,Patna-800020,Bihar.</address>
+            <address class="footer-list-item">MLR Colony Plot no 07. Rampally, Secundrabad, Hyderabad, 501301.</address>
           </li>
 
         </ul>
@@ -82,30 +84,45 @@
           </li>
 
           <li>
-            <p class="footer-list-item">Monday-Friday: 08:00-23:00</p>
+            <p class="footer-list-item">Monday-Sunday 11:00AM- 05:00PM </p>
           </li>
 
-          <li>
-            <p class="footer-list-item">Tuesday 4PM: Till Mid Night</p>
-          </li>
+        
 
-          <li>
-            <p class="footer-list-item">Saturday: 10:00-22:00</p>
-          </li>
+        
+        <li>
+            <a href="/privacy-policy" class="footer-list-item">Privacy & Policy</a>
+        </li>
 
-          <li>
-            <p class="footer-list-item">Privacy & Policy</p>
-          </li>
+        <li>
+            <a href="/terms-and-conditions" class="footer-list-item">Terms and Conditions</a>
+        </li>
 
+        <li>
+            <a href="/refund" class="footer-list-item">Refund Policy</a>
+        </li>
           
-          <li>
-            <p class="footer-list-item">Terms and conditions</p>
-          </li>
-
-
 
         </ul>
 
+
+
+
+
+
+
+        <ul class="footer-list">
+
+<li>
+  <p class="footer-list-title">Eatinsta Pos Billing </p>
+</li>
+
+
+ 
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d520.9804217379767!2d78.64689209018215!3d17.456047470498643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1716815234904!5m2!1sen!2sin" width="300" height="260" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+</ul>
 
 
 
@@ -119,7 +136,10 @@
     <div class="footer-bottom">
       <div class="container">
         <p class="copyright-text">
-          &copy; 2024 <a href="https://play.google.com/store/apps/details?id=org.com.callvcall" class="copyright-link">callvcal</a> All Rights Reserved.
+          &copy; 2024 <a href="/" class="copyright-link">CALLVCAL TECHNOLOGY PRIVATE LIMITED</a> All Rights Reserved.
+        </p>
+        <p class="copyright-text">
+         Powered by <a href="https://www.callvcal.com/" target="_blank" class="copyright-link">CALLVCAL CONSULTANCY</a> .
         </p>
       </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Eatplan8</title>
+    <link rel="shortcut icon" href="{{ asset('public/img/logo.jpg') }}" type="image/x-icon">
+    <title>EatInsta POS Billing - Elevate Your Business, Simplify Your Life! </title>
+
+  <meta property="og:title" content="EatInsta POS Billing - Elevate Your Business, Simplify Your Life!">
+  <meta property="og:description" content="Download Now and Experience the Future of Business Management!.">
+  <meta property="og:image" content="https://eatinsta.callvcal.com/public/img/heroi.png">
+  <meta property="og:url" content="https://www.callvcal.com">
+  <meta property="og:type" content="website">
+
+    
     <!-- Include Toastr CSS -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-
-<body>
 
 
 
