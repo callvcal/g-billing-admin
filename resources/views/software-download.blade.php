aff93a3f5c70b6e5d89cc8bf285c0852eeadc73f
Sonu Kumar <sonukumarsweet12@gmail.com>
2024-05-31 21:28:20 +0530

    donload

--- resources/views/software-download.blade.php
+++ resources/views/software-download.blade.php
@@ -1,4 +1,49 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ asset('public/img/logo.jpg') }}" type="image/x-icon">
+    <title>EatInsta POS Billing - Elevate Your Business, Simplify Your Life</title>
+
+    <!--
+    - favicon
+  -->
+    <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml">
+
+    <!--
+    - custom css link
+  -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <!--
+    - google font link
+  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha512-tIB+w1mt3v+eu+Cz6LxDMCeSXwCwY8QWZ46MfRmvPXvBwYwyX8/KuE18sLXqM7U11Sh1KkLtVLQPsS9siLDhfQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Rubik:wght@400;500;600;700&family=Shadows+Into+Light&display=swap"
+        rel="stylesheet">
+
+    <!--
+    - preload images
+  -->
+    <link rel="preload" as="image" href="{{ asset('img/hero-banner.png') }}" media="min-width: 768px">
+    <link rel="preload" as="image" href="{{ asset('img/hero-banner-bg.png') }}" media="min-width: 768px">
+    <link rel="preload" as="image" href="{{ asset('img/hero-bg.jpg') }}">
+
+</head>
+
+
+
+
 
 
 @include('body.header')
