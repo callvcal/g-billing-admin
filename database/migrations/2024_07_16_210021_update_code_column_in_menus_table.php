1c96a126ee3883c39b26391772ad977a2ae1455e
Callvcal <tech.callvcal@gmail.com>
2024-07-16 21:10:32 +0530

    save

--- database/migrations/2024_07_16_210021_update_code_column_in_menus_table.php
+++ database/migrations/2024_07_16_210021_update_code_column_in_menus_table.php
@@ -10,27 +10,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('menus', function (Blueprint $table) {
-            // Add a new column to temporarily store the auto-incremented values
-            $table->unsignedBigInteger('new_code')->nullable()->unique();
+         
+            
         });
-
-        // Populate new_code with incrementing values
-        DB::statement('SET @row_number = 0');
-        DB::statement('UPDATE menus SET new_code = (@row_number:=@row_number + 1)');
-
-        // Drop the old code column
-        Schema::table('menus', function (Blueprint $table) {
-            $table->dropColumn('code');
-        });
-
-        // Rename new_code to code and set it as auto-increment
-        Schema::table('menus', function (Blueprint $table) {
-            $table->unsignedBigInteger('new_code')->autoIncrement()->change();
-        });
-
-        Schema::table('menus', function (Blueprint $table) {
-            $table->renameColumn('new_code', 'code');
-        });
+        
     }
 
     /**
@@ -41,8 +24,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('menus', function (Blueprint $table) {
-            $table->dropColumn('code');
-            $table->string('code')->nullable();
+           
+            
         });
     }
 };
