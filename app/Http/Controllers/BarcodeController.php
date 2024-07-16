1843d90cb0007902ee84ba219b450921416b244a
Callvcal <tech.callvcal@gmail.com>
2024-07-16 21:50:04 +0530

    barcode

--- app/Http/Controllers/BarcodeController.php
+++ app/Http/Controllers/BarcodeController.php
@@ -10,26 +10,26 @@ use PhpParser\Node\Expr\Cast\String_;
 
 class BarcodeController extends Controller
 {
-    function genBarcode(Menu $menu) : bool {
+    function genBarcode(Menu $menu): bool
+    {
 
-        if(($menu)!=null){
-            if(isset($menu->code)){
+        if (($menu) != null) {
+            if (isset($menu->code)) {
                 return false;
             }
         }
-        
 
-        $barcode=MyBarcode::create();
-        $barcode->barcode_id=$barcode->id;
-        $barcode->barcode=(string)$barcode->id;
-        if(($menu)!=null){
-            $barcode->menu_id=$menu->id??null;
-            $menu->code=$barcode->barcode;
+
+        $barcode = MyBarcode::create();
+        $barcode->barcode_id = $barcode->id;
+        $barcode->barcode = ''.$barcode->id;
+        if (($menu) != null) {
+            $barcode->menu_id = $menu->id ?? null;
+            $menu->code = $barcode->barcode;
             $menu->save();
         }
         $barcode->save();
 
         return true;
-        
     }
 }
