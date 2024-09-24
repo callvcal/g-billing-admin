85908eaf83c80a4bb1567a0eaa688cc131139b05
Callvcal <tech.callvcal@gmail.com>
2024-09-24 16:53:16 +0530

    'id' => 2,

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -29,7 +29,7 @@ class SettingController extends Controller
     {
         $settings = Setting::updateOrCreate(
             [
-                'id' => 1,
+                'id' => 2,
             ],
 
             [
