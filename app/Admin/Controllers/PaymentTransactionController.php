5649b44741068eac915d241e39974cc471e911f9
Callvcal <tech.callvcal@gmail.com>
2024-11-16 14:09:06 +0530

    InAppPurchaseModelController

--- app/Admin/Controllers/PaymentTransactionController.php
+++ app/Admin/Controllers/PaymentTransactionController.php
@@ -6,17 +6,16 @@ use OpenAdmin\Admin\Controllers\AdminController;
 use OpenAdmin\Admin\Form;
 use OpenAdmin\Admin\Grid;
 use OpenAdmin\Admin\Show;
-use \App\Models\PaymentTransaction;
-use OpenAdmin\Admin\Facades\Admin;
+use \App\Models\InAppPurchaseModel;
 
-class PaymentTransactionController extends AdminController
+class InAppPurchaseModelController extends AdminController
 {
     /**
      * Title for current resource.
      *
      * @var string
      */
-    protected $title = 'PaymentTransaction';
+    protected $title = 'InAppPurchaseModel';
 
     /**
      * Make a grid builder.
@@ -25,35 +24,18 @@ class PaymentTransactionController extends AdminController
      */
     protected function grid()
     {
-        $grid = new Grid(new PaymentTransaction());
-
+        $grid = new Grid(new InAppPurchaseModel());
         $grid->model()->orderBy('id',"DESC");
-        $grid->column('id', __('Id'))->sortable();
-        $grid->column('order_id', __('Order id'))->sortable();
-        $grid->column('transaction_id', __('Transaction id'));
-        $grid->user()->display(function ($model) {
-            if ($model == null) {
-                return '<span class="badge rounded-pill bg-success">' . "N/A" . '</span> ';
-            }
-            return '<span class="badge rounded-pill bg-success">' . $model['name'] . '</span> ';
-        })->sortable();
+
+        $grid->column('id', __('Id'));
+        $grid->column('user_id', __('User id'));
         $grid->column('location_id', __('Location id'));
-        $grid->business()->display(function ($model) {
-            if ($model == null) {
-                return '<span class="badge rounded-pill bg-success">' . "N/A" . '</span> ';
-            }
-            return '<span class="badge rounded-pill bg-success">' . $model['name'] . '</span> ';
-        })->sortable();
-        $grid->column('location_id', __('Location id'))->sortable();
-        $grid->column('date_time', __('Date time'))->sortable();
-        $grid->column('transaction_status_local', __('Transaction status local'))->sortable();
-        $grid->column('transaction_status_callback', __('Transaction status callback'))->sortable();
-        $grid->column('plan', __('Plan'))->sortable();
-        $grid->column('amount', __('Amount'))->sortable();
-        $grid->column('gst', __('Gst'));
-        $grid->column('service_charge', __('Service charge'))->sortable();
-        $grid->column('created_at', __('Created at'))->sortable();
-        $grid->column('updated_at', __('Updated at'))->sortable();
+        // $grid->column('json', __('Json'));
+        $grid->column('status', __('Status'));
+        $grid->column('product_id', __('Product id'));
+        $grid->column('purchase_id', __('Purchase id'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
 
         return $grid;
     }
@@ -66,36 +48,15 @@ class PaymentTransactionController extends AdminController
      */
     protected function detail($id)
     {
-        $show = new Show(PaymentTransaction::findOrFail($id));
+        $show = new Show(InAppPurchaseModel::findOrFail($id));
 
         $show->field('id', __('Id'));
-        $show->field('order_id', __('Order id'));
-        $show->field('transaction_id', __('Transaction id'));
-        $show->user('User information', function ($model) {
-
-            $model->setResource('/admin/users');
-
-            $model->id();
-            $model->name();
-            $model->email();
-            $model->mobile();
-            $model->location();
-        });
-        $show->field('json', __('Json'))->json();
-        $show->business('business information', function ($model) {
-            $model->setResource('/admin/users');
-            $model->id();
-            $model->name();
-            $model->plan();
-        });
-        $show->field('date_time', __('Date time'));
-        $show->field('transaction_status_local', __('Transaction status local'));
-        $show->field('transaction_status_callback', __('Transaction status callback'));
-        $show->field('callback_json', __('Callback json'))->json();
-        $show->field('plan', __('Plan'));
-        $show->field('amount', __('Amount'));
-        $show->field('gst', __('Gst'));
-        $show->field('service_charge', __('Service charge'));
+        $show->field('user_id', __('User id'));
+        $show->field('location_id', __('Location id'));
+        $show->field('json', __('Json'));
+        $show->field('status', __('Status'));
+        $show->field('product_id', __('Product id'));
+        $show->field('purchase_id', __('Purchase id'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -109,23 +70,15 @@ class PaymentTransactionController extends AdminController
      */
     protected function form()
     {
-        $form = new Form(new PaymentTransaction());
+        $form = new Form(new InAppPurchaseModel());
 
-        $form->text('order_id', __('Order id'));
-        $form->text('transaction_id', __('Transaction id'));
         $form->number('user_id', __('User id'));
-        $form->text('json', __('Json'));
         $form->number('location_id', __('Location id'));
-        $form->datetime('date_time', __('Date time'))->default(date('Y-m-d H:i:s'));
-        $form->text('transaction_status_local', __('Transaction status local'));
-        $form->text('transaction_status_callback', __('Transaction status callback'));
-        $form->text('callback_json', __('Callback json'));
-        $form->text('plan', __('Plan'));
-        $form->decimal('amount', __('Amount'));
-        $form->decimal('gst', __('Gst'));
-        $form->decimal('service_charge', __('Service charge'));
-        $form->hidden('admin_id', __('Admin id'))->default(Admin::user()->id);
-        $form->hidden('business_id', __('Business id'))->default(Admin::user()->business_id);
+        $form->text('json', __('Json'));
+        $form->text('status', __('Status'));
+        $form->text('product_id', __('Product id'));
+        $form->text('purchase_id', __('Purchase id'));
+
         return $form;
     }
 }
