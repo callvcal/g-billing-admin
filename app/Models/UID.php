1a76b862e945d41e3386039c2b4a97a4917d723d
Callvcal <tech.callvcal@gmail.com>
2024-11-30 16:44:58 +0530

    generateUniqueUid

--- app/Models/UID.php
+++ app/Models/UID.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Ramsey\Uuid\Uuid;
 
 class UID extends Model
 {
@@ -20,9 +18,25 @@ class UID extends Model
         parent::boot();
 
         static::creating(function ($uid) {
-            // Generate UUID for the uid attribute
-            $uid->uid = Uuid::uuid4()->toString();
+            // Generate a unique 4-character alphanumeric string for the uid attribute
+            $uid->uid = self::generateUniqueUid();
         });
+    }
+
+    public static function generateUniqueUid()
+    {
+        // Define the character set: digits and lowercase letters
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        
+        // Generate a random 4-character string
+        $randomUid = substr(str_shuffle($characters), 0, 4);
+        
+        // Check if UID is unique
+        while (self::where('uid', $randomUid)->exists()) {
+            $randomUid = substr(str_shuffle($characters), 0, 4);
+        }
+
+        return $randomUid;
     }
 
     public function user()
